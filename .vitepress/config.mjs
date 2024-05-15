999ff03ebd58ed9c55c306b89e2a68092aa6583f
sole <macore@163.com>
2024-05-15 14:52:39 +0800

    feat: 添加ICP备案号

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -40,7 +40,7 @@ export default defineConfig({
 
     footer: {
       message: '基于 MIT 许可发布',
-      copyright: '版权所有 © 2024 julycms.com'
+      copyright: '版权所有 © 2024 julycms.com <a href="https://beian.miit.gov.cn" target="_blank">辽ICP备2024030417号-1</a>'
     },
 
     sitemap: {
